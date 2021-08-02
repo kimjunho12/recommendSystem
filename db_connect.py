ebfebcd8543aab6b5b97512cbeaaacdec3a9cc55
98kimjh <98kimjh@gmail.com>
2021-08-03 00:44:22 +0900

    로컬 모델로 변경

--- db_connect.py
+++ db_connect.py
@@ -6,10 +6,10 @@ from firebase_admin import db
 # %% # Firebase database 인증 및 앱 초기화
 
 cred = credentials.Certificate(
-    './andr/firebaseCert.json')
+    './andr/supportapp-f34a1-firebase-adminsdk-gzyie-87ed2eb9ba.json')
 
 firebase_admin.initialize_app(
-    cred, {'databaseURL': 'firebaseDBURL'})
+    cred, {'databaseURL': 'https://supportapp-f34a1-default-rtdb.firebaseio.com'})
 
 # %% 기본 위치 지정
 ref = db.reference('target')
@@ -24,12 +24,11 @@ target_df = pd.DataFrame(target).T.fillna('')
 # %%
 tmp = []
 for key, s in target_df.subject.iteritems():
-    tmp.append(pd.Series(data={'lCategory': s[0]['lCategory'],
-                               'mCategory': s[0]['mCategory'],
-                               'sCategory': s[0]['sCategory']},
+    tmp.append(pd.Series(data={'lCategory': s['lCategory'],
+                               'mCategory': s['mCategory'],
+                               'sCategory': s['sCategory']},
                          name=key))
-    print(key, s[0]['lCategory'] + ' ' + s[0]
-          ['mCategory'] + ' ' + s[0]['sCategory'])
+    print(key, s['lCategory'] + ' ' + s['mCategory'] + ' ' + s['sCategory'])
 subject_df = pd.DataFrame(tmp)
 subject_df.to_csv('./data/target-subject.csv')
 # %%
--- makeModel.py
+++ makeModel.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras.utils import to_categorical
-
+# %%
 subject_df = pd.read_csv('./data/target-subject.csv', index_col=0)
 subject_text_df = pd.read_csv('./data/target-subject.csv', index_col=0)
 subject_text_df['target'] = subject_text_df.index
@@ -22,20 +22,14 @@ target = subject_df['target']
 
 target_tt = to_categorical(target)
 
-
-def return_target(intclasses):
-    for intclass in intclasses:
-        print('-'*10)
-        for x in intclass[::-1][:10]:
-            print(target.index[x])
-
+# %%
 
 inputs = keras.Input(shape=(None,), dtype=tf.string, name='Input_Layer')
 indexer = preprocessing.StringLookup(
     output_mode='binary', name='One_Hot_Layer')
 indexer.adapt(subject_text_df)
 x = indexer(inputs)
-classer = layers.Dense(26, activation='softmax', name='Output_Layer')
+classer = layers.Dense(len(target), activation='softmax', name='Output_Layer')
 outputs = classer(x)
 model = keras.Model(inputs=inputs, outputs=outputs)
 
@@ -43,19 +37,33 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy']
               )
-
+# %%
 model.fit(subject_text_df, target_tt, epochs=400)
 
-model.save('./model/keras_model')
-converter = tf.lite.TFLiteConverter.from_saved_model('./model/keras_model/')
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.inference_input_type = tf.string
-converter.inference_input_type = tf.float32
-converter.target_spec.supported_types = [tf.string, tf.resource]
-converter.target_spec.supported_ops = []
-converter.target_spec.supported_ops.append(tf.lite.OpsSet.TFLITE_BUILTINS)
-converter.target_spec.supported_ops.append(tf.lite.OpsSet.SELECT_TF_OPS)
-tflitemodel = converter.convert()
+# %%
+# 결과값 이름으로 도출 [:n] - n 개 만큼
+def return_target(intclasses):
+    for intclass in intclasses:
+        print('-'*10)
+        for x in intclass[::-1][:10]:
+            print(target.index[x])
+# %%
+return_target(np.argsort(model.predict([['K-pop'], ['하키']])))
+return_target(np.argsort(model.predict([['K-pop', '서울과학기술대학교']])))
 
-with open('./model/contents_base_model.tflite', 'wb') as f:
-  f.write(tflitemodel)
+# %%
+
+# # %%
+# model.save('./model/keras_model')
+# converter = tf.lite.TFLiteConverter.from_saved_model('./model/keras_model/')
+# converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# converter.inference_input_type = tf.string
+# converter.inference_input_type = tf.float32
+# converter.target_spec.supported_types = [tf.string, tf.resource]
+# converter.target_spec.supported_ops = []
+# converter.target_spec.supported_ops.append(tf.lite.OpsSet.TFLITE_BUILTINS)
+# converter.target_spec.supported_ops.append(tf.lite.OpsSet.SELECT_TF_OPS)
+# tflitemodel = converter.convert()
+
+# with open('./model/contents_base_model.tflite', 'wb') as f:
+#   f.write(tflitemodel)
